0659f25beb16f8273be6e10b66895e4b89bc4c66
Shashi Gharti <shashi.gharti@gmail.com>
2020-05-21 12:48:53 +0545

    Added test for get_metrics function

--- tests/test_binomial.py
+++ tests/test_binomial.py
@@ -13,3 +13,17 @@ def test_generate_events():
     assert len(events) == 10
 
 
+def test_get_metrics():
+    events = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1], 
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 1, 0, 1, 1, 1, 1, 1, 1, 1]]
+    reviews_per_observation, positive_reviews = 10, 8
+    assert  binomial_distribution.get_metrics(events, reviews_per_observation, positive_reviews) == ['10', '10', '8', '10.00%']
